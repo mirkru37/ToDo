c26fe5fe451d31b6da6bc4db8677441d915e74e5
Roman Myrka <roman_myrka@epam.com>
2022-11-02 21:58:48 +0200

    Add Models *temp*

---
+++ Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Model
+{
+    internal class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Task_Category> Task_Categories { get; set; }
+    }
+}
+
