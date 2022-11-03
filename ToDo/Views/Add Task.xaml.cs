d8f7606589b1e458dfefaa294a12cf90d7b0f405
OlexandraKarbovska <113400473+OlexandraKarbovska@users.noreply.github.com>
2022-11-03 12:57:15 +0200

    Реалізований дизайн (окрім статистики)

---
+++ ToDo/Views/Add Task.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace ToDo.Views;
+
+public partial class Add_Task : Window
+{
+    public Add_Task()
+    {
+        InitializeComponent();
+    }
+}
---
+++ ToDo/Views/Day.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace ToDo.Views;
+
+public partial class Day : Window
+{
+    public Day()
+    {
+        InitializeComponent();
+    }
+}
